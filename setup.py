c9218aaa7d8e915c7bc420d8a1e67ae1264d7237
Lucas Andrade <lafdesouza2002@outlook.com>
2024-02-06 16:41:50 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-url-analyzer',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     author='Lucas Andrade',
     author_email='lafdesouza2002@gmail.com',
